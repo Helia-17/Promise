a16829e6169447bfc5a3711aa16c9d49d55f3211
이가빈(Ariel) <kv829ly@naver.com>
2021-10-19 21:17:06 +0900

    :tada: [master] backend init project

---
+++ backend/src/main/java/com/pjt3/promise/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.pjt3.promise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pjt3/promise/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjt3.promise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
