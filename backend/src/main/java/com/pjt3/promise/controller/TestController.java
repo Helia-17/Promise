d681944015702e77fbb559d14aaac092b6598d15
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-10-24 18:50:02 +0900

    :sparkles: TestController 생성

---
+++ backend/src/main/java/com/pjt3/promise/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.pjt3.promise.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @GetMapping()
+    public ResponseEntity<String> test(){
+        return ResponseEntity.status(200).body("테스트가 정상적으로 작동합니다.");
+    }
+
+}
