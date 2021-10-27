0b702e437389ca9b5c646c702e3295cdeae5edc5
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-10-28 01:03:43 +0900

    :recycle: [BE_YS] Test Message

--- backend/src/main/java/com/pjt3/promise/controller/TestController.java
+++ backend/src/main/java/com/pjt3/promise/controller/TestController.java
@@ -11,7 +11,7 @@ public class TestController {
 
     @GetMapping()
     public ResponseEntity<String> test(){
-        return ResponseEntity.status(200).body("도커 이미지, 젠킨스 테스트가 정상적으로 작동합니다.");
+        return ResponseEntity.status(200).body("도커 이미지, 젠킨스 테스트가 정상적으로 작동합니다. 22");
     }
 
 }
