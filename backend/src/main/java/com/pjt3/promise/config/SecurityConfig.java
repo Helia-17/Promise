a042b12b35be3100ed8d09d62af783386a66fd2f
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-10-29 00:18:16 +0900

    :sparkles: [BE_YS] add JWT Config Logic

--- backend/src/main/java/com/pjt3/promise/config/SecurityConfig.java
+++ backend/src/main/java/com/pjt3/promise/config/SecurityConfig.java
@@ -13,17 +13,21 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.pjt3.promise.common.auth.JwtAuthenticationFilter;
+import com.pjt3.promise.common.auth.PMUserDetailsService;
+import com.pjt3.promise.service.UserService;
+
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Autowired
-//    LBUserDetailsService lbUserDetailsService;
+    @Autowired
+    PMUserDetailsService pmUserDetailsService;
 
-//    @Autowired
-//    private UserService userService;
+    @Autowired
+    private UserService userService;
 
     // Password 인코딩 방식으로 BCrypt 암호화 방식 사용
     @Bean
@@ -34,19 +38,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // DAO 기반으로 Authentication Provider를 생성
     // BCrypt Password Encoder와 UserDetailService 구현체를 설정
 
-//    @Bean
-//    DaoAuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-//        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-//        daoAuthenticationProvider.setUserDetailsService(this.lbUserDetailsService);
-//        return daoAuthenticationProvider;
-//    }
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(this.pmUserDetailsService);
+        return daoAuthenticationProvider;
+    }
 
-    //	//DAO 기반의 Authentication Provider가 적용되도록 설정
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) {
-//        auth.authenticationProvider(authenticationProvider());
-//    }
+	//DAO 기반의 Authentication Provider가 적용되도록 설정
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
@@ -55,7 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
                 .and()
-//                .addFilter(new JwtAuthenticationFilter(authenticationManager(), userService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
+                .addFilter(new JwtAuthenticationFilter(authenticationManager(), userService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
                 .authorizeRequests()
                 .antMatchers("api/**").authenticated()//인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
                 .anyRequest().permitAll()
