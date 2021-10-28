888b5c6e6dd8c8661b25d78ecb7deaa31ecafb56
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-10-28 23:36:45 +0900

    :recycle: [BE_YS] modify UserRepository

--- backend/src/main/java/com/pjt3/promise/repository/UserRepository.java
+++ backend/src/main/java/com/pjt3/promise/repository/UserRepository.java
@@ -9,4 +9,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, String> {
     User findUserByUserEmail(String userEmail);
     User findUserByUserNickname(String userNickname);
+    User findUserByRefreshToken(String refreshToken);
 }
