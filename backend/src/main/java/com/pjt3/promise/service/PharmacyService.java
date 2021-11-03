b8297737b2951b81a9164dd6ab1f0899268012dd
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-11-03 23:54:36 +0900

    :recycle: [BE_YS] modify PharmacyService

--- backend/src/main/java/com/pjt3/promise/service/PharmacyService.java
+++ backend/src/main/java/com/pjt3/promise/service/PharmacyService.java
@@ -2,10 +2,8 @@ package com.pjt3.promise.service;
 
 import java.util.List;
 
-import com.pjt3.promise.entity.Pharmacy;
 import com.pjt3.promise.response.PharmacyGetRes;
 
 public interface PharmacyService {
-//	List<PharmacyGetRes> getPharmacyList(String pharmAddrCity, String pharmAddrGu);
 	List<PharmacyGetRes> getPharmacyListByLatLong(double lat, double lon);
 }
