21be2c459eeb181c9254e24a5384e0006617008b
허애리 <hureari@naver.com>
2021-11-07 23:35:48 +0900

    :sparkles: [FE_AR] alarm add page

--- promise_app/src/components/SearchBar/index.js
+++ promise_app/src/components/SearchBar/index.js
@@ -8,7 +8,7 @@ const SearchBar = () => {
     return (
         <View style={{width:'90%', height:40, margin: 10, alignItems: 'center', justifyContent: 'center', backgroundColor: '#EEEEEE', borderRadius: 20}}>
             <View style={{ alignItems: 'center', flexDirection: "row"}}>
-                <TextInput onChangeText={onChangeText} value={text} style={{width:280, backgroundColor: '#EEEEEE', borderRadius: 20}}/>
+                <TextInput onChangeText={onChangeText} value={text} style={{width:280, backgroundColor: '#EEEEEE', color:'black', borderRadius: 20}}/>
                 <Icon.Button onPress={()=>alert('검색!!')} name="search" color="black" backgroundColor='#EEEEEE' size={20}/>
             </View>
         </View>
