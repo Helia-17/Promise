d45d2ec369cee2c46cd4800d5ba6ff2675f63e57
허애리 <hureari@naver.com>
2021-11-09 16:18:16 +0900

    :recycle: [FE_AR] Google Login Client ID 분리

--- promise_app/src/components/GoogleLoginBtn/index.js
+++ promise_app/src/components/GoogleLoginBtn/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import {View} from 'react-native';
 import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-community/google-signin';
+import GOOGLE_LOGIN_ID from '../../utils/oauth';
 
-GoogleSignin.configure({
-  // 숨겨야할 정보: webClientId, androidClientId, iosClientId
-  webClientId: '82314462051-h7k16fkq06mct82meic3cav3o5lub21r.apps.googleusercontent.com',
-  androidClientId: '82314462051-3fhhej98qvu1hrlkith0s9afqv1f8voq.apps.googleusercontent.com',
-  iosClientId: '82314462051-oem6e03vfa5jo412gpfqbn5h2bgqtrrg.apps.googleusercontent.com',
-  offlineAccess: true,
-})
+GoogleSignin.configure({GOOGLE_LOGIN_ID});
 
 const GoogleLoginBtn = (props) => {
 
--- promise_app/src/utils/oauth/index.js
+++ promise_app/src/utils/oauth/index.js
@@ -1,1 +1,7 @@
-export const GOOGLE_GEO_API = 'AIzaSyBhj3tc6-OLHV0mJqqPfRGp4xWVX0J29mc';
+export const GOOGLE_LOGIN_ID = {
+    // 숨겨야할 정보: webClientId, androidClientId, iosClientId
+    webClientId: '82314462051-h7k16fkq06mct82meic3cav3o5lub21r.apps.googleusercontent.com',
+    androidClientId: '82314462051-3fhhej98qvu1hrlkith0s9afqv1f8voq.apps.googleusercontent.com',
+    iosClientId: '82314462051-oem6e03vfa5jo412gpfqbn5h2bgqtrrg.apps.googleusercontent.com',
+    offlineAccess: true,
+  };
