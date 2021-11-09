7f86f70338a28bd990de91070774ffe0312be283
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2021-11-10 04:34:09 +0900

    :sparkles: [FE_JY] 커뮤니티 목록 뷰

--- promise_app/src/pages/Community/index.js
+++ promise_app/src/pages/Community/index.js
@@ -2,20 +2,18 @@ import React, {useState, useEffect} from 'react';
 import { View, ScrollView, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import RoundBtn from '../../components/atoms/RoundBtn'; 
+import SearchBar from '../../components/community/SearchBar';
+import PostList from '../../components/community/PostList';
 
 const CommunityPage = ({navigation}) => {
     
     return (
-        <View  style={{ flex: 1, alignItems: 'center', backgroundColor:'#F9F9F9', justifyContent:'center' }}>
-            <ScrollView style={{ width:'100%', margin:10}} contentContainerStyle={{alignItems: 'center', margin:10}}>
-                <Text>Community</Text>
-                <View style={{width:'90%', margin:10}}>
-                    <TouchableOpacity style={{backgroundColor:'#A3BED7', color:'black', alignItems: 'center', borderRadius: 12, height:50, justifyContent: 'center'}} onPress={()=>alert('등록이얌')}>
-                        <Text style={{color:'black', fontSize:20, fontWeight:'bold'}}>등록하기</Text >
-                    </TouchableOpacity>
-                </View>
-
-            </ScrollView>
+        <View  style={{ flex: 1, alignItems: 'center', backgroundColor:'#F9F9F9' }}>
+            {/* search bar */}
+            <SearchBar/>
+            <View style={{width:'100%', margin:10}}>
+                <PostList/>
+            </View>
             <View style={{width:'100%', alignItems:'flex-end', position: 'absolute', left: 0, right: 0, bottom: 0}}>
                 <RoundBtn 
                     func={()=>navigation.navigate('PostCreate')}
