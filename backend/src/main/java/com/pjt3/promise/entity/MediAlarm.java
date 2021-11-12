944490d314a4c98283729f7bc9d0fa74c49eaa37
이가빈(Ariel) <kv829ly@naver.com>
2021-11-12 14:09:48 +0900

    :recycle: [BE_GV] update MediAlarm Entity (add alarmTime2/3 columns)

--- backend/src/main/java/com/pjt3/promise/entity/MediAlarm.java
+++ backend/src/main/java/com/pjt3/promise/entity/MediAlarm.java
@@ -42,8 +42,14 @@ public class MediAlarm {
     @Column(name="alarm_YN")
     int alarmYN;
 
-    @Column(name="alarm_time")
-    String alarmTime;
+    @Column(name="alarm_time1")
+    String alarmTime1;
+    
+    @Column(name="alarm_time2")
+    String alarmTime2;
+    
+    @Column(name="alarm_time3")
+    String alarmTime3;
 
     @Column(name="alarm_day_start")
     String alarmDayStart;
