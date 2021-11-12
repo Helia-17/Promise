16119d4ec07e77e39ff34b25db5de12855f2d0c5
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-11-12 22:13:34 +0900

    :sparkles:[BE_YS] 주변 약국 거리 반환

--- backend/src/main/java/com/pjt3/promise/service/PharmacyServiceImpl.java
+++ backend/src/main/java/com/pjt3/promise/service/PharmacyServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pjt3.promise.entity.Pharmacy;
+import com.pjt3.promise.entity.PharmacyDistanceInterface;
 import com.pjt3.promise.repository.PharmacyRepository;
 import com.pjt3.promise.repository.PharmacyRepositorySupport;
 import com.pjt3.promise.response.PharmacyGetRes;
@@ -24,19 +25,23 @@ public class PharmacyServiceImpl implements PharmacyService {
 	@Override
 	public List<PharmacyGetRes> getPharmacyListByLatLong(double lat, double lon, int week, String curTime) {
 		List<PharmacyGetRes> pharmacyListGetRes = new ArrayList<>();
-		List<Pharmacy> pharmacyList = new ArrayList<>();
+		List<PharmacyDistanceInterface> pharmacyList = new ArrayList<>();
 		
 		pharmacyList = pharmacyRepositorySupport.getPharmacyList(lat, lon, week, curTime);
 		
-		for (Pharmacy pharmacy : pharmacyList) {
+		
+		for (PharmacyDistanceInterface pharmacy : pharmacyList) {
 			PharmacyGetRes pharmacyGetRes = new PharmacyGetRes();
-			
+			Double distance = pharmacy.getDistance() * 1000;
+			int intDistance = distance.intValue();
+					
 			pharmacyGetRes.setPharmId(pharmacy.getPharmId());
 			pharmacyGetRes.setPharmName(pharmacy.getPharmName());
 			pharmacyGetRes.setPharmAddr(pharmacy.getPharmAddr());
 			pharmacyGetRes.setPharmTel(pharmacy.getPharmTel());
 			pharmacyGetRes.setPharmLat(pharmacy.getPharmLat());
 			pharmacyGetRes.setPharmLong(pharmacy.getPharmLong());
+			pharmacyGetRes.setDistance(intDistance);
 			
 			pharmacyListGetRes.add(pharmacyGetRes);
 		}
