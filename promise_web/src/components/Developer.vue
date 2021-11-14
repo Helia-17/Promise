530d78e21f530070e6506bd1e432134ae51b11df
이가빈(Ariel) <kv829ly@naver.com>
2021-11-14 15:53:58 +0900

    :tada: [WEB] init web vue project

---
+++ promise_web/src/components/Developer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>Developer</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+.container{
+  width: 100%;
+  height: 100%;
+  background-color: #DBDFE2;
+}
+</style>
---
+++ promise_web/src/components/Intro.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class ="container">
+    <p>더 건강한 나를 위한 약속</p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+.container{
+  width: 100%;
+  height: 100%;
+  background-color: #DBDFE2;
+}
+</style>
