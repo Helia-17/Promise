140d7baf578b8d99ef608611c4aa78c868fd45cf
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2021-11-14 20:12:48 +0900

    :bug: [FE_JY] 마이필에서 route.params를 넘겨주면서 약정보로 넘어갔다가 마이필로 돌아오는게 되지 않고 마이페이지로 돌아가는 문제

--- promise_app/src/navigations/NowPillNav/index.js
+++ promise_app/src/navigations/NowPillNav/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Info from '../../pages/Info';
 import Search from '../../pages/Search';
 import MyPillNowPill from '../../pages/MyPillNowPill';
 import { createNativeStackNavigator  } from '@react-navigation/native-stack';
@@ -6,14 +7,27 @@ import { createNativeStackNavigator  } from '@react-navigation/native-stack';
 
 const NowPillNav = () => {
 
-    const Stack = createNativeStackNavigator();
+	const Stack = createNativeStackNavigator();
 
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="MyPillNowPill" component={MyPillNowPill} options={{ headerShown : false }}/>
-            <Stack.Screen name="Search" component={Search} options={{ headerShown : false }}/>
-        </Stack.Navigator>
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name="MyPillNowPill" component={MyPillNowPill} options={{ headerShown : false }}/>
+			<Stack.Screen name="NowPillInfoNav" component={NowPillInfoNav} options={{ headerShown : false }} />
+			
+		</Stack.Navigator>
     )
+}
+
+const NowPillInfoNav  = ({route}) => {
+	const Stack = createNativeStackNavigator();
+
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name="MyPillInfo" options={{ headerShown : false }} >
+				{() => <Info route={route}/>}
+			</Stack.Screen>
+		</Stack.Navigator>
+	)
 }
 
 export default NowPillNav;
