c73258de30b5273fc96b84c07dd872db762cf1b2
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-11-16 03:45:01 +0900

    :recycle: [BE_YS] 홈 로딩 스피너 제거

--- promise_app/src/pages/Home/index.js
+++ promise_app/src/pages/Home/index.js
@@ -4,7 +4,6 @@ import Carousel from '../../components/Carousel';
 import {myinfo} from '../../utils/axios';
 import { getMyInfoAction } from '../../modules/user/actions';
 import { useDispatch } from 'react-redux';
-import Spinner from 'react-native-loading-spinner-overlay';
 
 const screenWidth = Math.round(Dimensions.get('window').width);
 const PAGES = [1];
@@ -12,14 +11,11 @@ const PAGES = [1];
 const HomePage = ({navigation}) => {
     
     const dispatch = useDispatch();
-    const [isVisible, setIsvisible] = useState();
     const [userInfo, setUserInfo] = useState({});
 
     const getMyInfo = ()=>{
-        setIsvisible(true);
         return myinfo()
         .then(res => {
-            setIsvisible(false);
             if(res.statusCode === 420) navigation.replace('LoginScreen');
             else{
                 setUserInfo(res);
@@ -30,13 +26,12 @@ const HomePage = ({navigation}) => {
 
     useEffect(()=>{
         getMyInfo()
-      }, [])
+    }, [])
 
     
 
     return (
         <View  style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'#F9F9F9' }}>
-            <Spinner visible={isVisible} />
             <Carousel
             gap={0}
             offset={0}
