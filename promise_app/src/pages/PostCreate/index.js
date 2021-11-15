9f5f73cb05d95bae60c5b712601a822afde2283a
허애리 <hureari@naver.com>
2021-11-15 23:15:48 +0900

    :recycle: [FE_AR] modify style

--- promise_app/src/pages/PostCreate/index.js
+++ promise_app/src/pages/PostCreate/index.js
@@ -1,10 +1,6 @@
-import React, {useState, useEffect} from 'react';
-import { View, ScrollView, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
-
+import React, {useState} from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { getCommunityAPI } from '../../utils/axios';
-
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; 
-import InputText from '../../components/InputText';
 import InputLongText from '../../components/InputLongText';
 import InputTitleText from '../../components/InputTitleText';
 
@@ -14,45 +10,43 @@ const PostCreatePage = ({navigation}) => {
 
     const sendPost = () => {
         getCommunityAPI.create(title, content).then(res => {
-            // console.log(res)
             navigation.navigate('community', {created: true});
           })
     }
     
     return (
-        <>
+        <View>
             <View  style={styles.titleView}>
                 <View style={{width:'90%'}}>
                     <InputTitleText name='제목' text='' result={(data)=>onChangeTitle(data)} />
                 </View>
             </View>
             <View  style={styles.mainView}>
-                <ScrollView style={{ width:'90%' }} contentContainerStyle={{alignItems: 'center', justifyContent: 'flex-start'}}>
+                <View style={{ width:'90%', alignItems: 'center', justifyContent:'flex-start'}} >
                     <View style={{width:'100%', margin:10}}>
                         <InputLongText name='내용' text='' result={(data)=>onChangeContent(data)} />
                     </View>
-                </ScrollView>
+                </View>
                 <View style={{width:'90%', margin:10, alignItmes:'flex-end'}}>
                     <TouchableOpacity style={{backgroundColor:'#A3BED7', color:'black', alignItems: 'center', borderRadius: 12, height:50, justifyContent: 'center'}} onPress={sendPost}>
                         <Text style={{color:'black', fontSize:20, fontWeight:'bold'}}>작성</Text >
                     </TouchableOpacity>
                 </View>
             </View>
-        </>
+        </View>
 
 
     );
 };
 
 const styles = StyleSheet.create({
-    titleView: { 
-        flex: 1, 
+    titleView: {
+        marginTop:20, 
         alignItems: 'center', 
         backgroundColor:'#F9F9F9', 
         justifyContent:'center' 
     },
     mainView: { 
-        flex: 6, 
         alignItems: 'center', 
         backgroundColor:'#F9F9F9', 
         justifyContent:'center' 
