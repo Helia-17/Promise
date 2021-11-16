a8a3e8005f836096a212a38c8b76116dad8d2171
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-11-16 19:52:47 +0900

    :recycle: [BE_YS] 알람 내용 스타일링

--- promise_app/src/components/atoms/MediInfo/index.js
+++ promise_app/src/components/atoms/MediInfo/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const MediInfo = (props) => {
     return (
-        <View style={{width:'93%', marginTop:10, backgroundColor:'white', borderRadius:3, elevation:1}}>
+        <View style={style.mediInfoContainer}>
             <TouchableOpacity style={{width:'100%', height:80, justifyContent: 'center'}} onPress={()=>{}}>
                 <View style={{flexDirection: "row", alignItems:'center', justifyContent: 'space-between', margin:15}}>
                     <Text style={{color:'black', fontWeight:'bold', fontSize:18}}>{props.alarmTitle}</Text>
@@ -13,4 +13,16 @@ const MediInfo = (props) => {
         </View>
     );
 };
+
+const style = StyleSheet.create({
+    mediInfoContainer: {
+        width: '93%',
+        marginTop: 10,
+        borderWidth: 0.3,
+        borderColor: '#BBBBBB',
+        borderRadius: 5,
+        backgroundColor: 'white',
+        elevation: 1
+    }
+})
 export default MediInfo;
