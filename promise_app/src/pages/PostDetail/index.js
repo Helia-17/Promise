8894bdc8af9bee2cf15861e9576463710adc28f8
김지윤(Jiyun Kim) <76979903+Helia-17@users.noreply.github.com>
2021-11-18 14:48:50 +0900

    :bug: [FE_JY] 커뮤니티 접속시 마운트 이후에도 리로드되도록 수정

--- promise_app/src/pages/PostDetail/index.js
+++ promise_app/src/pages/PostDetail/index.js
@@ -1,4 +1,5 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
+import { useFocusEffect } from '@react-navigation/core';
 import {
   View,
   ScrollView,
@@ -38,9 +39,11 @@ const PostDetailPage = ({navigation, route}) => {
         })
   }
 
-  useEffect(()=> {
-    setUserNickname(stateUserNickname)
-  }, [])
+  useFocusEffect(
+    useCallback(()=>{
+      setUserNickname(stateUserNickname)
+    }, [])
+);
 
 
   const getCommunityDetail = () => {
